d19c7080c79e28f29bd5c467c367e3c91bf7423d
Jared Hanson <jaredhanson@gmail.com>
2012-11-03 11:54:22 -0700

    Deserialize 0 value serialized to session.

--- test/strategies/session-test.js
+++ test/strategies/session-test.js
@@ -92,6 +92,49 @@ vows.describe('SessionStrategy').addBatch({
     },
   },
   
+  'strategy handling a request with a login session using user ID 0': {
+    topic: function() {
+      return new SessionStrategy();
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.pass = function() {
+          self.callback(null, req);
+        }
+        strategy.error = function(err) {
+          self.callback(err, req);
+        }
+        
+        req._passport = {};
+        req._passport.instance = {};
+        req._passport.instance.deserializeUser = function(user, done) {
+          done(null, { id: user });
+        }
+        req._passport.session = {};
+        req._passport.session.user = 0;
+        
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, req) {
+        assert.isNull(err);
+      },
+      'should set a user on the request' : function(err, req) {
+        assert.isObject(req.user);
+        assert.equal(req.user.id, 0);
+      },
+      'should maintain the session' : function(err, req) {
+        assert.isObject(req._passport.session);
+        assert.equal(req._passport.session.user, 0);
+      },
+    },
+  },
+  
   'strategy handling a request with a login session that has been invalidated': {
     topic: function() {
       return new SessionStrategy();
