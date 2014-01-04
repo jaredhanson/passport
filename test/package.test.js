5dfce167bd16bb063b21ec0efe7624b1ea629797
Jared Hanson <jaredhanson@gmail.com>
2014-01-04 09:58:08 -0800

    Add mocha test case for package exports.

---
+++ test/package.test.js
@@ -0,0 +1,21 @@
+/* global describe, it, expect */
+
+var passport = require('..');
+
+describe('passport', function() {
+  
+  it('should expose singleton authenticator', function() {
+    expect(passport).to.be.an('object');
+  });
+  
+  it('should export constructors', function() {
+    expect(passport.Authenticator).to.equal(passport.Passport);
+    expect(passport.Authenticator).to.be.a('function');
+    expect(passport.Strategy).to.be.a('function');
+  });
+  
+  it('should export strategies', function() {
+    expect(passport.strategies.SessionStrategy).to.be.a('function');
+  });
+  
+});
