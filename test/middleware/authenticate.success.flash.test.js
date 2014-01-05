17b4086b5dcb4eb226d6b05f22a8ad76ba879d6b
Jared Hanson <jaredhanson@gmail.com>
2014-01-04 17:30:22 -0800

    Port authenticate middleware test cases to mocha.

--- test/middleware/authenticate.success.flash.test.js
+++ test/middleware/authenticate.success.flash.test.js
@@ -266,6 +266,58 @@ describe('middleware/authenticate', function() {
   });
   
   
+  describe('success with flash message and type set by strategy but message overridden by route as string', function() {
+    function Strategy() {
+    }
+    Strategy.prototype.authenticate = function(req, options) {
+      var user = { id: '1', username: 'jaredhanson' };
+      this.success(user, { type: 'info', message: 'Hello' });
+    }    
+    
+    var passport = new Passport();
+    passport.use('success', new Strategy());
+    
+    var request, response;
+
+    before(function(done) {
+      chai.connect.use('express', authenticate('success', { successFlash: 'Success!',
+                                                            successRedirect: 'http://www.example.com/account' }).bind(passport))
+        .req(function(req) {
+          request = req;
+          req.session = {};
+          
+          req.logIn = function(user, options, done) {
+            this.user = user;
+            done();
+          }
+          req.flash = function(type, msg) {
+            this.message = { type: type, msg: msg }
+          }
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should set user', function() {
+      expect(request.user).to.be.an('object');
+      expect(request.user.id).to.equal('1');
+      expect(request.user.username).to.equal('jaredhanson');
+    });
+    
+    it('should flash message', function() {
+      expect(request.message.type).to.equal('success');
+      expect(request.message.msg).to.equal('Success!');
+    });
+    
+    it('should redirect', function() {
+      expect(response.statusCode).to.equal(302);
+      expect(response.getHeader('Location')).to.equal('http://www.example.com/account');
+    });
+  });
+  
   describe('success with flash message and type set by strategy', function() {
     function Strategy() {
     }
