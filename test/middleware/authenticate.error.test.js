d46b05196865e51ec94e7b02d2fbe290925d7497
Jared Hanson <jaredhanson@gmail.com>
2014-01-05 14:05:37 -0800

    Test cases for strategy that errors.

---
+++ test/middleware/authenticate.error.test.js
@@ -0,0 +1,42 @@
+var chai = require('chai')
+  , authenticate = require('../../lib/passport/middleware/authenticate')
+  , Passport = require('../..').Passport;
+
+
+describe('middleware/authenticate', function() {
+  
+  describe('error', function() {
+    function Strategy() {
+    }
+    Strategy.prototype.authenticate = function(req) {
+      this.error(new Error('something is wrong'));
+    }    
+    
+    var passport = new Passport();
+    passport.use('error', new Strategy());
+    
+    var request, error;
+
+    before(function(done) {
+      chai.connect.use(authenticate('error').bind(passport))
+        .req(function(req) {
+          request = req;
+        })
+        .next(function(err) {
+          error = err;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should error', function() {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('something is wrong');
+    });
+    
+    it('should not set user', function() {
+      expect(request.user).to.be.undefined;
+    });
+  });
+  
+});
