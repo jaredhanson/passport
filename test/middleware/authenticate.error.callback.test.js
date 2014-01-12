4a8230cc7d246a6085b12b6f5d0943cd2c01aae1
Jared Hanson <jaredhanson@gmail.com>
2014-01-12 09:31:15 -0800

    Pass error to custom callback, if supplied.

---
+++ test/middleware/authenticate.error.callback.test.js
@@ -0,0 +1,88 @@
+var chai = require('chai')
+  , authenticate = require('../../lib/passport/middleware/authenticate')
+  , Passport = require('../..').Passport;
+
+
+describe('middleware/authenticate', function() {
+  
+  describe('error with callback', function() {
+    function Strategy() {
+    }
+    Strategy.prototype.authenticate = function(req) {
+      this.error(new Error('something is wrong'));
+    }    
+    
+    var passport = new Passport();
+    passport.use('error', new Strategy());
+    
+    var request, error, user;
+
+    before(function(done) {
+      function callback(e, u) {
+        error = e;
+        user = u;
+        done();
+      }
+      
+      chai.connect.use(authenticate('error', callback).bind(passport))
+        .req(function(req) {
+          request = req;
+        })
+        .dispatch();
+    });
+    
+    it('should pass error to callback', function() {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('something is wrong');
+    });
+    
+    it('should pass user as undefined to callback', function() {
+      expect(request.user).to.be.undefined;
+    });
+    
+    it('should not set user on request', function() {
+      expect(request.user).to.be.undefined;
+    });
+  });
+  
+  describe('error with callback and options passed to middleware', function() {
+    function Strategy() {
+    }
+    Strategy.prototype.authenticate = function(req) {
+      this.error(new Error('something is wrong'));
+    }    
+    
+    var passport = new Passport();
+    passport.use('error', new Strategy());
+    
+    var request, error, user;
+
+    before(function(done) {
+      function callback(e, u) {
+        error = e;
+        user = u;
+        done();
+      }
+      
+      chai.connect.use(authenticate('error', { foo: 'bar' }, callback).bind(passport))
+        .req(function(req) {
+          request = req;
+        })
+        .dispatch();
+    });
+    
+    it('should pass error to callback', function() {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('something is wrong');
+    });
+    
+    it('should pass user as undefined to callback', function() {
+      expect(request.user).to.be.undefined;
+    });
+    
+    it('should not set user on request', function() {
+      expect(request.user).to.be.undefined;
+    });
+  });
+  
+});
