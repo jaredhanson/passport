7ad41f27b3703fc1d85458558189689f32401214
Jared Hanson <jaredhanson@gmail.com>
2014-01-25 09:09:34 -0800

    Add test cases.

--- test/authenticator.middleware.test.js
+++ test/authenticator.middleware.test.js
@@ -31,6 +31,52 @@ describe('Authenticator', function() {
       it('should not error', function() {
         expect(error).to.be.undefined;
       });
+      
+      it('should set user property on authenticator', function() {
+        expect(passport._userProperty).to.equal('user')
+      });
+    
+      it('should initialize namespace within session', function() {
+        expect(request.session.passport).to.be.an('object');
+        expect(Object.keys(request.session.passport)).to.have.length(0);
+      });
+    
+      it('should expose authenticator on internal request property', function() {
+        expect(request._passport).to.be.an('object');
+        expect(request._passport.instance).to.be.an.instanceOf(Authenticator);
+        expect(request._passport.instance).to.equal(passport);
+      });
+    
+      it('should expose session storage on internal request property', function() {
+        expect(request._passport.session).to.be.an('object');
+        expect(Object.keys(request._passport.session)).to.have.length(0);
+      });
+    });
+    
+    describe('handling a request with custom user property', function() {
+      var passport = new Authenticator();
+      var request, error;
+
+      before(function(done) {
+        chai.connect.use(passport.initialize({ userProperty: 'currentUser' }))
+          .req(function(req) {
+            request = req;
+            req.session = {};
+          })
+          .next(function(err) {
+            error = err;
+            done();
+          })
+          .dispatch();
+      });
+    
+      it('should not error', function() {
+        expect(error).to.be.undefined;
+      });
+      
+      it('should set user property on authenticator', function() {
+        expect(passport._userProperty).to.equal('currentUser')
+      });
     
       it('should initialize namespace within session', function() {
         expect(request.session.passport).to.be.an('object');
@@ -161,6 +207,55 @@ describe('Authenticator', function() {
     
       it('should not set authInfo', function() {
         expect(request.authInfo).to.be.undefined;
+      });
+    });
+    
+  });
+  
+  describe('#session', function() {
+    
+    it('should have correct arity', function() {
+      var passport = new Authenticator();
+      expect(passport.session).to.have.length(1);
+    });
+    
+    describe('handling a request', function() {
+      var passport = new Authenticator();
+    
+      var request, error;
+
+      before(function(done) {
+        chai.connect.use(passport.session())
+          .req(function(req) {
+            request = req;
+            
+            req._passport = {};
+            req._passport.instance = {};
+            req._passport.instance.deserializeUser = function(user, req, done) {
+              done(null, { id: user });
+            };
+            req._passport.session = {};
+            req._passport.session.user = '123456';
+          })
+          .next(function(err) {
+            error = err;
+            done();
+          })
+          .dispatch();
+      });
+    
+      it('should not error', function() {
+        expect(error).to.be.undefined;
+      });
+    
+      it('should set user', function() {
+        expect(request.user).to.be.an('object');
+        expect(request.user.id).to.equal('123456');
+      });
+      
+      it('should maintain session', function() {
+        expect(request._passport.session).to.be.an('object');
+        expect(request._passport.session.user).to.equal('123456');
       });
     });
     
