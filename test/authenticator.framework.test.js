bd384ebdb13076a8cd406ff875154e8a5abc4052
Jared Hanson <jaredhanson@gmail.com>
2014-01-25 09:41:55 -0800

    Test case for Authenticator#framework.

---
+++ test/authenticator.framework.test.js
@@ -0,0 +1,54 @@
+var chai = require('chai')
+  , Authenticator = require('../lib/authenticator');
+
+
+describe('Authenticator', function() {
+  
+  describe('#framework', function() {
+    
+    describe('with an authenticate function used for authorization', function() {
+      var passport = new Authenticator();
+      passport.framework({
+        initialize: function() {
+          return function() {};
+        },
+        authenticate: function(name, options) {
+          return function() {
+            return 'authenticate(): ' + name + ' ' + options.assignProperty;
+          };
+        }
+      });
+      
+      var rv = passport.authorize('foo')();
+      it('should call authenticate', function() {
+        expect(rv).to.equal('authenticate(): foo account');
+      });
+    });
+    
+    describe('with an authorize function used for authorization', function() {
+      var passport = new Authenticator();
+      passport.framework({
+        initialize: function() {
+          return function() {};
+        },
+        authenticate: function(name, options) {
+          return function() {
+            return 'authenticate(): ' + name + ' ' + options.assignProperty;
+          };
+        },
+        authorize: function(name, options) {
+          return function() {
+            return 'authorize(): ' + name + ' ' + options.assignProperty;
+          };
+        }
+      });
+      
+      var rv = passport.authorize('foo')();
+      it('should call authorize', function() {
+        expect(rv).to.equal('authorize(): foo account');
+      });
+    });
+    
+  });
+  
+});
