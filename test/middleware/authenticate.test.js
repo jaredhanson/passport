c0966333215a9e82b7724ce8abd0c1f01c6088a3
Jared Hanson <jaredhanson@gmail.com>
2014-01-25 08:59:59 -0800

    Clarify error message.

--- test/middleware/authenticate.test.js
+++ test/middleware/authenticate.test.js
@@ -1,11 +1,48 @@
 var chai = require('chai')
-  , authenticate = require('../../lib/middleware/authenticate');
+  , authenticate = require('../../lib/middleware/authenticate')
+  , Passport = require('../..').Passport;
 
 
 describe('middleware/authenticate', function() {
   
   it('should be named authenticate', function() {
     expect(authenticate().name).to.equal('authenticate');
+  });
+  
+  describe('with unknown strategy', function() {
+    var passport = new Passport();
+    
+    var request, error;
+
+    before(function(done) {
+      chai.connect.use(authenticate('foo').bind(passport))
+        .req(function(req) {
+          request = req;
+          
+          req.logIn = function(user, options, done) {
+            this.user = user;
+            done();
+          }
+        })
+        .next(function(err) {
+          error = err;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should not error', function() {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('Unknown authentication strategy "foo"');
+    });
+    
+    it('should not set user', function() {
+      expect(request.user).to.be.undefined;
+    });
+    
+    it('should not set authInfo', function() {
+      expect(request.authInfo).to.be.undefined;
+    });
   });
   
 });
