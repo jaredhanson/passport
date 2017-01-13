7200ccc8cf4fcd61aee44bae515172e2d7a4a25b
Cam Jackson <camjackson89@gmail.com>
2017-01-13 11:30:29 +1100

    Add redirectStatus option to passport.authenticate

--- test/middleware/authenticate.fail.test.js
+++ test/middleware/authenticate.fail.test.js
@@ -56,7 +56,7 @@ describe('middleware/authenticate', function() {
     var request, response;
 
     before(function(done) {
-      chai.connect.use('express', authenticate(passport, 'fail', { failureRedirect: 'http://www.example.com/login' }))
+      chai.connect.use('express', authenticate(passport, 'fail', { failureRedirect: 'http://www.example.com/login', redirectStatus: 303 }))
         .req(function(req) {
           request = req;
         })
@@ -72,7 +72,7 @@ describe('middleware/authenticate', function() {
     });
     
     it('should redirect', function() {
-      expect(response.statusCode).to.equal(302);
+      expect(response.statusCode).to.equal(303);
       expect(response.getHeader('Location')).to.equal('http://www.example.com/login');
     });
   });
