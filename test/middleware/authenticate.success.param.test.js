95c60485288ee5c485c51604f1bd3f348ecbabc5
Jorrit Schippers <jorrit@ncode.nl>
2017-04-27 19:52:59 +0200

    Read strategy name from request parameter
    This adds the option `nameFromParam` to the authenticate method.
    Set this to a parameter name that is set by the application to
    dynamically configure the strategy to use.
    
    Example:
    
    app.get('/login/:name(facebook|google)/callback',
      passport.authenticate(null, { nameFromParam: 'name' })

---
+++ test/middleware/authenticate.success.param.test.js
@@ -0,0 +1,107 @@
+/* global describe, it, expect, before */
+/* jshint expr: true */
+
+var chai = require('chai')
+  , authenticate = require('../../lib/middleware/authenticate')
+  , Passport = require('../..').Passport;
+
+
+describe('middleware/authenticate', function() {
+  
+  describe('with multiple strategies, the first is selected using a param', function() {
+    function StrategyA() {
+    }
+    StrategyA.prototype.authenticate = function(req) {
+      this.success({ username: 'bob-a' });
+    };
+    
+    function StrategyB() {
+    }
+    StrategyB.prototype.authenticate = function(req) {
+      this.success({ username: 'bob-b' });
+    };
+    
+    var passport = new Passport();
+    passport.use('a', new StrategyA());
+    passport.use('b', new StrategyB());
+    
+    var request, error;
+
+    before(function(done) {
+      chai.connect.use(authenticate(passport, null, { nameFromParam: 'name' }))
+        .req(function(req) {
+          request = req;
+          
+          req.params = { name: 'a' };
+          
+          req.logIn = function(user, options, done) {
+            this.user = user;
+            done();
+          };
+        })
+        .next(function(err) {
+          error = err;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should not error', function() {
+      expect(error).to.be.undefined;
+    });
+    
+    it('should set user', function() {
+      expect(request.user).to.be.an('object');
+      expect(request.user.username).to.equal('bob-a');
+    });
+  });
+  
+  describe('with multiple strategies, the second is selected using a param', function() {
+    function StrategyA() {
+    }
+    StrategyA.prototype.authenticate = function(req) {
+      this.fail('A challenge');
+    };
+    
+    function StrategyB() {
+    }
+    StrategyB.prototype.authenticate = function(req) {
+      this.success({ username: 'bob-b' });
+    };
+    
+    var passport = new Passport();
+    passport.use('a', new StrategyA());
+    passport.use('b', new StrategyB());
+    
+    var request, error;
+
+    before(function(done) {
+      chai.connect.use(authenticate(passport, null, { nameFromParam: 'name' }))
+        .req(function(req) {
+          request = req;
+
+          req.params = { name: 'b' };
+
+          req.logIn = function(user, options, done) {
+            this.user = user;
+            done();
+          };
+        })
+        .next(function(err) {
+          error = err;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should not error', function() {
+      expect(error).to.be.undefined;
+    });
+    
+    it('should set user', function() {
+      expect(request.user).to.be.an('object');
+      expect(request.user.username).to.equal('bob-b');
+    });
+  });
+  
+});
