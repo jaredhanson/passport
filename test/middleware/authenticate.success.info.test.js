f189da2b428b691c2618afa93fb7e911b195e1aa
Samuel Judson <sam@sjudson.com>
2018-07-29 09:49:01 -0500

    Add test for flagging off of new assignProperty + authInfo behavior.

--- test/middleware/authenticate.success.info.test.js
+++ test/middleware/authenticate.success.info.test.js
@@ -201,5 +201,54 @@ describe('middleware/authenticate', function() {
       expect(request.authInfo).to.be.undefined;
     });
   });
+
+  describe('success that assigns property and with info, but option that disables info', function() {
+    function Strategy() {
+    }
+    Strategy.prototype.authenticate = function(req) {
+      var user = { id: '1', username: 'jaredhanson' };
+      this.success(user, { clientId: '123', scope: 'read' });
+    };
+
+    var passport = new Passport();
+    passport.use('success', new Strategy());
+
+    var request, error;
+
+    before(function(done) {
+      chai.connect.use(authenticate(passport, 'success', { assignProperty: 'account', authInfo: false }))
+        .req(function(req) {
+          request = req;
+
+          req.logIn = function(user, options, done) {
+            this.user = user;
+            done();
+          };
+        })
+        .next(function(err) {
+          error = err;
+          done();
+        })
+        .dispatch();
+    });
+
+    it('should not error', function() {
+      expect(error).to.be.undefined;
+    });
+
+    it('should not set user', function() {
+      expect(request.user).to.be.undefined;
+    });
+
+    it('should set account', function() {
+      expect(request.account).to.be.an('object');
+      expect(request.account.id).to.equal('1');
+      expect(request.account.username).to.equal('jaredhanson');
+    });
+
+    it('should not set authInfo', function() {
+      expect(request.authInfo).to.be.undefined;
+    });
+  });
   
 });
