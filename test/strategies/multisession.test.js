b79f81a35ddf37df0ff0232ebc97003ad1dcc14b
Jared Hanson <jaredhanson@gmail.com>
2021-10-08 15:21:43 -0700

    Implement multi option.

--- test/strategies/multisession.test.js
+++ test/strategies/multisession.test.js
@@ -190,4 +190,66 @@ describe('MultiSessionStrategy', function() {
     });
   });
   
+  describe.only('handling a request with multiple login sessions and multi option', function() {
+    var strategy = new MultiSessionStrategy(function(user, req, done) {
+      done(null, user);
+    });
+    
+    var request, pass = false;
+  
+    before(function(done) {
+      chai.passport.use(strategy)
+        .pass(function() {
+          pass = true;
+          done();
+        })
+        .req(function(req) {
+          request = req;
+          
+          req._passport = {};
+          req._passport.instance = {};
+          req.session = {};
+          req.session['passport'] = {};
+          req.session['passport'][0] = {};
+          req.session['passport'][0].user = { id: '123456', displayName: 'Alice' };
+          req.session['passport'][1] = {};
+          req.session['passport'][1].user = { id: '123457', displayName: 'Bob' };
+        })
+        .authenticate({ multi: true });
+    });
+  
+    it('should pass', function() {
+      expect(pass).to.be.true;
+    });
+    
+    it('should set user on request', function() {
+      expect(request.user).to.deep.equal([ {
+        id: '123456',
+        displayName: 'Alice'
+      }, {
+        id: '123457',
+        displayName: 'Bob'
+      } ]);
+    });
+    
+    it('should set authInfo on request', function() {
+      expect(request.authInfo).to.deep.equal([ {
+        selector: '0'
+      }, {
+        selector: '1'
+      } ]);
+    });
+    
+    it('should maintain session', function() {
+      expect(request.session['passport']).to.deep.equal({
+        0: {
+          user: { id: '123456', displayName: 'Alice' }
+        },
+        1: {
+          user: { id: '123457', displayName: 'Bob' }
+        }
+      });
+    });
+  });
+  
 });
